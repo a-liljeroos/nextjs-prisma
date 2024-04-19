c5524b45132da7ff60bf6b22d85cec16014759d9
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-04-19 16:18:56 +0300

    delete globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
+"use client";
+// auth
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      {session && (
+        <div className="text-xs user-select-none">
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+        </div>
+      )}
+    </main>
   );
 }
