21d602d01d0da4dd7d13f28b99397fec7c87f783
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-12 18:32:06 +0300

    user/post/write/page.tsx - write post form a dynamic form. includes header, paragraph inputs the user can add more subheaders and paragraphs

---
+++ src/app/user/post/write/writePostContext.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+export type PostContent = {
+  index: number;
+  type: "Header" | "Subheader" | "Paragraph";
+  content: string;
+  inputId?: string;
+};
+
+export type InputType = { type: "Subheader" | "Paragraph"; inputId: string };
+
+interface WritePostContextProps {
+  submitLock: boolean;
+  setSubmitLock: (submitLock: boolean) => void;
+  extraInputFields: InputType[];
+  setExtraInputFields: (extraInputFields: InputType[]) => void;
+  newInputIndex?: () => number;
+  removeExtraInputField: (inputId: string) => void;
+  moveItem: (item: InputType, direction: "up" | "down") => void;
+  postContent: PostContent[];
+  updatePostContent: (content: PostContent) => void;
+}
+
+const WritePostContext = createContext<WritePostContextProps>(
+  {} as WritePostContextProps
+);
+
+export const useWritePostContext = () => {
+  return useContext(WritePostContext);
+};
+
+type WritePostContextProviderProps = {
+  children: ReactNode;
+};
+
+export const WritePostContextProvider = ({
+  children,
+}: WritePostContextProviderProps) => {
+  const [postContent, setPostContent] = useState<PostContent[]>([
+    {
+      index: 0,
+      type: "Header",
+      content: "",
+    },
+    {
+      index: 1,
+      type: "Paragraph",
+      content: "",
+    },
+  ]);
+
+  const updatePostContent = (content: PostContent) => {
+    const staleRemoved = postContent.filter((item) => {
+      if (item.index !== content.index) {
+        return item;
+      }
+    });
+    const contentSorted = [...staleRemoved, cleanPostContent(content)].sort(
+      (a, b) => a.index - b.index
+    );
+    setPostContent(contentSorted);
+  };
+
+  const [submitLock, setSubmitLock] = useState(true);
+
+  const [extraInputFields, setExtraInputFields] = useState<InputType[]>([]);
+
+  const removeExtraInputField = (inputId: string) => {
+    const newExtraInputFields = extraInputFields.filter((inputField) => {
+      if (inputField.inputId !== inputId) {
+        return inputField;
+      }
+    });
+    setExtraInputFields(newExtraInputFields);
+
+    const remainingPostContent = postContent.filter((item) => {
+      if (item.inputId !== inputId) {
+        return item;
+      }
+    });
+    setPostContent(remainingPostContent);
+  };
+
+  const moveItem = (item: InputType, direction: "up" | "down") => {
+    /* let items = [...extraInputFields];
+    const currentIndex = items.indexOf(item);
+    const temp = items[currentIndex];
+    if (direction === "up" && currentIndex < items.length - 1) {
+      items[currentIndex] = items[currentIndex + 1];
+      items[currentIndex + 1] = temp;
+    }
+    if (direction === "down" && currentIndex >= 3) {
+      items[currentIndex] = items[currentIndex - 1];
+      items[currentIndex - 1] = temp;
+    }
+    setExtraInputFields(items); */
+  };
+
+  return (
+    <WritePostContext.Provider
+      value={{
+        submitLock,
+        setSubmitLock,
+        extraInputFields,
+        setExtraInputFields,
+        removeExtraInputField,
+        moveItem,
+        postContent,
+        updatePostContent,
+      }}
+    >
+      {children}
+    </WritePostContext.Provider>
+  );
+};
+
+const cleanPostContent = (postContent: PostContent): PostContent => {
+  let cleanedContent = postContent.content;
+
+  cleanedContent = cleanedContent.trim();
+
+  return { ...postContent, content: cleanedContent };
+};
