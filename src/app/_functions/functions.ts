67d1d21db150b01a02189a0209fc33dc4a331eb9
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-12 17:33:31 +0300

    _functions/functions.ts - random id generator

--- src/app/_functions/functions.ts
+++ src/app/_functions/functions.ts
@@ -1,3 +1,10 @@
+import { randomBytes } from "crypto";
+
+export function randomId(): string {
+  const id = randomBytes(10).toString("hex");
+  return id;
+}
+
 export default function formatDate(dateInput: Date): string {
   const date = new Date(dateInput);
   const formattedDate = `${date.getHours()}:${date.getMinutes()} ${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`;
