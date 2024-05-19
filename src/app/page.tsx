4834d956724687eb8f7b51b4c621c833d8b2d067
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-19 18:34:24 +0300

    src/app/page.tsx - postList on the front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 // auth
 import { useSession } from "next-auth/react";
-import GlitchText from "./_components/glitchText/GlitchText";
+import GlitchText from "@components/glitchText/GlitchText";
+import FrontPagePosts from "@/app/_components/post/frontPagePosts";
 
 export default function Home() {
   const { data: session, status } = useSession();
   return (
-    <main className="flex flex-col items-center p-24">
-      <GlitchText text="  " />
-      <GlitchText text="Hi       " />
-      <GlitchText text="There... " />
-      <GlitchText text="    " />
+    <main className="flex flex-col items-center pt-10">
+      <div className="pt-6">
+        <div className="pl-6">
+          <GlitchText text="  " />
+          <GlitchText text="Hi       " />
+          <GlitchText text="There... " />
+          <GlitchText text="    " />
+        </div>
+        <div className="mt-4">
+          <FrontPagePosts />
+        </div>
+      </div>
     </main>
   );
 }
