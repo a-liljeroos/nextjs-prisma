7738e32c3b94a8d2316a9dcd12415d96caf373cc
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-26 13:01:45 +0300

    (client)/[user]/edit/page.tsx - redirect boundary

--- src/app/(client)/[user]/edit/page.tsx
+++ src/app/(client)/[user]/edit/page.tsx
@@ -1,6 +1,14 @@
+import { auth } from "@serverAuth";
+import { redirect } from "next/navigation";
+// components
 import EditProfile from "./editProfile";
 
-const Page = () => {
+const Page = async ({ params }: { params: { user: string } }) => {
+  const session = await auth();
+
+  if (!session || session.user?.name !== params.user) {
+    return redirect("/");
+  }
   return <EditProfile />;
 };
 
