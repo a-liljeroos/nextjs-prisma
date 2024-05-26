6067ad87f888a8bb5171d46ad3b22e975fb14ba5
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-26 19:04:00 +0300

    [user]/edit/changeUsername - input default value

--- src/app/(client)/[user]/edit/_components/changeUsername.tsx
+++ src/app/(client)/[user]/edit/_components/changeUsername.tsx
@@ -7,7 +7,7 @@ import InputLabel from "@components/formComponents/inputLabel";
 import EditProfileListItem from "./editProfileListItem";
 
 const ChangeUsername = () => {
-  const { formList } = useEditProfileContext();
+  const { formList, userData } = useEditProfileContext();
   const inputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     if (inputRef.current) {
@@ -18,6 +18,9 @@ const ChangeUsername = () => {
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
+
+  const name = userData ? userData.name : "";
+
   return (
     <EditProfileListItem indexNo={2} name="Change Username">
       <form
@@ -26,7 +29,13 @@ const ChangeUsername = () => {
         className="flex flex-col gap-3"
       >
         <InputLabel name="New username" />
-        <input ref={inputRef} type="text" placeholder="New username" />
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="New username"
+          defaultValue={name}
+          autoCorrect="off"
+        />
         <button type="submit" value="change-username" className="mt-4">
           Change username
         </button>
