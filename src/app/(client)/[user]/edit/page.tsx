5c22fede2e89312a72df2c21da38fe5fa5ae545e
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-26 19:01:19 +0300

    [user]/edit/editProfile.tsx - fetch profile data

--- src/app/(client)/[user]/edit/page.tsx
+++ src/app/(client)/[user]/edit/page.tsx
@@ -1,15 +1,40 @@
 import { auth } from "@serverAuth";
 import { redirect } from "next/navigation";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import getProfile from "../_fetchProfile/getProfile";
 // components
 import EditProfile from "./editProfile";
 
 const Page = async ({ params }: { params: { user: string } }) => {
-  const session = await auth();
+  try {
+    const session = await auth();
+    const user = params.user;
+    const sessionUser = session?.user?.name;
 
-  if (!session || session.user?.name !== params.user) {
+    if (!session || sessionUser !== user) {
+      return redirect("/");
+    }
+
+    const queryClient = new QueryClient();
+
+    await queryClient.prefetchQuery({
+      queryKey: ["profile", sessionUser],
+      queryFn: () => getProfile({ name: sessionUser }),
+    });
+
+    return (
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <EditProfile user={sessionUser} />
+      </HydrationBoundary>
+    );
+  } catch (error) {
+    console.error("[user]/edit", error);
     return redirect("/");
   }
-  return <EditProfile />;
 };
 
 export default Page;
