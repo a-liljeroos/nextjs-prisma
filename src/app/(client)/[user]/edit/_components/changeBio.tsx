2c90c486c524151a5786a3106faedd335ff29a70
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-26 18:59:13 +0300

    [user]/edit/_components/changeBio - form submit fn

--- src/app/(client)/[user]/edit/_components/changeBio.tsx
+++ src/app/(client)/[user]/edit/_components/changeBio.tsx
@@ -1,21 +1,53 @@
 "use client";
 import React, { useEffect, useRef, FormEvent } from "react";
+// react-query
+import { useMutation } from "@tanstack/react-query";
+import useGetProfile from "../../_fetchProfile/useGetProfile";
 // context
 import { useEditProfileContext } from "../editProfileContext";
 // components
 import EditProfileListItem from "./editProfileListItem";
+import toast from "react-hot-toast";
 
 const ChangeBio = () => {
   const bioRef = useRef<HTMLTextAreaElement>(null);
-  const { formList } = useEditProfileContext();
+  const { formList, userData, user } = useEditProfileContext();
+  const { refetch } = useGetProfile(user);
+  const bio = userData ? userData.profile?.bio : "";
+  const userId = userData ? userData.id : null;
+
+  const mutation = useMutation({
+    mutationFn: (data: { bio: string; id: number | null }) => {
+      return fetch(`/api/user/profile/update/bio`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      });
+    },
+    onSettled: (data) => {
+      if (data?.status === 201) {
+        toast.success("Bio updated.", { duration: 4000 });
+        refetch();
+      }
+      if (data?.status === 500) {
+        toast.error("Try again later.");
+      }
+    },
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const bio = formData.get("bio") as string;
+
+    mutation.mutate({ bio: bio, id: userId });
+  };
+
   useEffect(() => {
     if (bioRef.current) {
       bioRef.current.focus();
     }
   }, [formList]);
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-  };
 
   return (
     <EditProfileListItem name="Write bio" indexNo={0}>
@@ -25,9 +57,13 @@ const ChangeBio = () => {
         className="flex flex-col gap-3"
       >
         <textarea
+          name="bio"
           ref={bioRef}
-          className="max-h-96 min-h-36"
+          defaultValue={bio ? bio : ""}
+          maxLength={76}
+          className="max-h-36 min-h-36"
           placeholder="Write something about yourself..."
+          autoCorrect="off"
         />
         <button type="submit" value="Change bio" className="mt-2">
           Update
