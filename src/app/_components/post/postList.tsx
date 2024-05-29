f3aa3a82a1fcfde305ff1799d47528f3fa0819b6
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-05-30 00:25:49 +0300

    components/post/postList.tsx - render animation

--- src/app/_components/post/postList.tsx
+++ src/app/_components/post/postList.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import { useEffect } from "react";
 // auth
 import { useSession } from "next-auth/react";
+// functions
+import { getPoints } from "@functions";
 // components
 import PostOperations from "./postOperations";
 // styles
 import "./postList.scss";
 
-interface PostListProps {
+type PostListProps = {
   name?: string;
+  animate?: boolean;
   posts:
     | {
         id: number;
@@ -18,11 +22,12 @@ interface PostListProps {
       }[]
     | null
     | undefined;
-}
+};
 
-const PostList = ({ posts, name }: PostListProps) => {
+const PostList = ({ posts, name, animate = true }: PostListProps) => {
   const { data: session } = useSession();
   const isOwner = name ? session?.user?.name === name : false;
+  const delayValues = getPoints({ a: 0.3, n: posts!.length }) || [];
 
   return (
     <div className="posts-cont">
@@ -31,30 +36,64 @@ const PostList = ({ posts, name }: PostListProps) => {
       )}
       {posts && (
         <ul>
-          {posts.map((post) => {
-            const authorName = name ? name : post.author?.name;
+          {posts.map((post, index) => {
+            const del = delayValues[index].y;
             return (
-              <li
-                key={post.id}
-                className="flex items-center justify-between px-3 font-semibold hover:bg-backgroundSecondary hover:text-background"
-              >
-                <Link href={`/${authorName}/${post.id}`}>
-                  <div className="flex items-baseline">
-                    <h2>{post.title}</h2>
-                  </div>
-                </Link>
-                {isOwner && (
-                  <PostOperations
-                    postId={post.id.toString()}
-                    user={authorName}
-                  />
-                )}
-              </li>
+              <ListItem
+                post={post}
+                isOwner={isOwner}
+                name={name}
+                delay={del}
+                animate={animate}
+              />
             );
           })}
         </ul>
       )}
     </div>
+  );
+};
+
+interface ListItemProps {
+  post: {
+    id: number;
+    title: string;
+    author?: { name: string };
+  };
+  isOwner: boolean;
+  name?: string;
+  delay: number;
+  animate?: boolean;
+}
+
+const ListItem = ({
+  post,
+  isOwner,
+  name,
+  delay,
+  animate = true,
+}: ListItemProps) => {
+  const authorName = name ? name : post.author?.name;
+  const animationClass = animate ? "" : "animate-post-list-item";
+
+  return (
+    <li
+      key={post.id}
+      className={
+        "post-list-item flex items-center justify-between px-3 font-semibold " +
+        animationClass
+      }
+      style={{ animationDelay: `${delay}s` }}
+    >
+      <Link href={`/${authorName}/${post.id}`}>
+        <div className="flex items-baseline">
+          <h2>{post.title}</h2>
+        </div>
+      </Link>
+      {isOwner && (
+        <PostOperations postId={post.id.toString()} user={authorName} />
+      )}
+    </li>
   );
 };
 
