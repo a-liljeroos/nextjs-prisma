5346f2496829e5ace7d47c0f5ec5d97ae28bda75
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-08-08 20:00:52 +0300

    src/app/_actions/actions.ts - delete avatar

--- src/app/_actions/actions.ts
+++ src/app/_actions/actions.ts
@@ -1,10 +1,24 @@
 "use server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import prisma from "@prisma/prismaClient";
 
 export async function revalidatePost(url: string) {
   revalidatePath(url);
 }
 export async function redirectToPost(url: string) {
   redirect(url);
+}
+
+export async function deleteAvatar(userId: number) {
+  try {
+    await prisma.profile.update({
+      where: { userId: userId },
+      data: { image: null },
+    });
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
