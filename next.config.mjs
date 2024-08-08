31588bf8d3d7f62eebe06151d144d8d8effed87e
user-5039623452 <83167019+user-5039623452@users.noreply.github.com>
2024-08-08 20:01:59 +0300

    next.config - avatar image source

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif"],
+    domains: ["hf1ttc1z4ihjagr5.public.blob.vercel-storage.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hf1ttc1z4ihjagr5.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/avatars/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
